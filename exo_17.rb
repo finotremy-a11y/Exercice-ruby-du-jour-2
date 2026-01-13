745743264085c70158657982fa9613a6432f1fd0
finotremy-a11y <finotremy@gmail.com>
2026-01-13 13:18:52 +0100

    dix septieme exo

---
+++ exo_17.rb
@@ -0,0 +1,12 @@
+print "Salut, voici ma super pyramide, combien veux-tu d'etages ? : "
+n = gets.chomp.to_i
+
+if n <= 0 || n >= 26
+  puts "Nombre invalide (doit être > 0 mais < 26)."
+else
+  1.upto(n) do |i|
+    spaces = n - i
+    stars = "#" * (2 * i - 1)
+    puts " " * spaces + stars
+  end
+end
