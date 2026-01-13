cb1af3a5bf68cfa579270a0ebc6817f861311ede
finotremy-a11y <finotremy@gmail.com>
2026-01-13 10:21:26 +0100

    dixieme exo

---
+++ exo_10.rb
@@ -0,0 +1,11 @@
+require 'date'
+
+print "Quelle est votre année de naissance ? "
+birth_year = gets.chomp.to_i
+current_year = Date.today.year
+
+(birth_year..current_year).each do |year|
+  age = year - birth_year
+  age_label = age == 1 ? "an" : "ans"
+  puts "#{year} — #{age} #{age_label}"
+end
